51d518d1acf2e325290c9b24f8abe0f9067c0316
Kwame Gyamfi <kwame.gyamfi@akimtech.com>
2026-06-03 17:59:12 -0400

    changed alt tags from openclaw to max

--- ui/src/ui/app-render.ts
+++ ui/src/ui/app-render.ts
@@ -1730,14 +1730,10 @@ export function renderApp(state: AppViewState) {
                 ${navCollapsed
                   ? nothing
                   : html`
-                      <img
-                        class="sidebar-brand__logo"
-                        src="${agentLogoUrl(basePath)}"
-                        alt="OpenClaw"
-                      />
+                      <img class="sidebar-brand__logo" src="${agentLogoUrl(basePath)}" alt="Max" />
                       <span class="sidebar-brand__copy">
                         <span class="sidebar-brand__eyebrow">${t("nav.control")}</span>
-                        <span class="sidebar-brand__title">OpenClaw</span>
+                        <span class="sidebar-brand__title">Max</span>
                       </span>
                     `}
               </div>
